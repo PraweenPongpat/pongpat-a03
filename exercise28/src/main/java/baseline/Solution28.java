e5b5bd411e669881e9390127b89893f674642898
Praween Pongpat <praween.pongpat@gmail.com>
2021-10-01 11:26:10 -0400

    draft idea ex28

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,47 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Praween Pongpat
+ */
+
+package baseline;
+
+public class Solution28 {
+    private static final int NUMBERS = 5;
+
+    public static void main(String[] args) {
+        //allocate Solution28
+        Solution28 sol = new Solution28();
+
+        //make am array to store numbers
+        double[] numArr = new double[NUMBERS];
+
+        //make a counted loop
+        for(int i = 0; i< NUMBERS; i++){
+            //each iteration, get a numerical input
+        }
+
+        //find sum
+        double sum = sol.calcTotal(numArr);
+
+        //display output
+        System.out.println();
+    }
+
+    public double calcTotal(double[] numArr) {
+        //counted loop go through array
+        for(int i = 0; i<numArr.length; i++){
+            //each iteration, find the sum
+        }
+        //return the sum
+        return 0;
+    }
+
+    private double getDouble() {
+        //prompt user for number
+        //read input
+        //validate input must be numerical, do not let go further till numerical is entered
+        //return that number
+        return 0;
+    }
+
+}
